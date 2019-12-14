39417eb04d5af489cdad01a8ea76141ecc981a51
azxs <azxs>
2019-12-14 18:41:12 +0530

    deleted few lines

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-let a = 12;
-let b = 34;
-
-console.log(`The addition of ${a} & ${b} is ${a + b}`);
-
-let guestList = "Guests:\n * John\n * Pete\n * Mary";
-
-
-console.log(guestList); // a multiline list of guests
-
 console.log( "\u00A9" ); // ©
 console.log( "\u{20331}" ); // 佫, a rare Chinese hieroglyph (long unicode)
 console.log( "\u{1F60D}" ); // 😍, a smiling face symbol (another long unicode)
